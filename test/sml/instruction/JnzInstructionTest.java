84fffd6f6a077714c30f308ce20fe086e45ea6b5
andrewwildgoose <a.wildgoose@yahoo.co.uk>
2023-02-19 11:47:17 +0000

    JnzInstructionTest now working. Still need to add toString(), equals() and hashcode() tests to all Instruction tests.

--- test/sml/instruction/JnzInstructionTest.java
+++ test/sml/instruction/JnzInstructionTest.java
@@ -16,22 +16,12 @@ import static sml.Registers.Register.*;
 class JnzInstructionTest {
   private Machine machine;
   private Registers registers;
-  private Instruction inst1;
-  private Instruction inst2;
-  private Instruction inst3;
-  private List<Instruction> program;
 
   @BeforeEach
   void setUp() {
     machine = new Machine(new Registers());
     registers = machine.getRegisters();
-    inst1 = new AddInstruction(null, EAX, EBX);
-    inst2 = new SubInstruction("f3", EAX, ECX);
-    inst3 = new JnzInstruction(null, EAX, "f3");
-    program = new ArrayList<>();
-    program.add(inst1);
-    program.add(inst2);
-    program.add(inst3);
+    machine.getLabels().addLabel("f3", 2);
     //...
   }
 
@@ -39,28 +29,19 @@ class JnzInstructionTest {
   void tearDown() {
     machine = null;
     registers = null;
-    program = null;
-    inst1 = null;
-    inst2 = null;
-    inst3 = null;
   }
 
   @Test
   void executeValid() {
     registers.set(EAX, 0);
-    registers.set(EBX, 5);
-    registers.set(ECX, 3);
     Instruction instruction = new JnzInstruction(null, EAX, "f3");
-    instruction.execute(machine);
-    Assertions.assertEquals("f3", machine);
+    Assertions.assertEquals(-1, instruction.execute(machine));
   }
 
   @Test
   void executeValidTwo() {
-    registers.set(EAX, -5);
-    registers.set(EBX, 6);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
-    instruction.execute(machine);
-    Assertions.assertEquals(1, machine.getRegisters().get(EAX));
+    registers.set(EBX, 5);
+    Instruction instruction = new JnzInstruction(null, EBX, "f3");
+    Assertions.assertEquals(2, instruction.execute(machine));
   }
 }
